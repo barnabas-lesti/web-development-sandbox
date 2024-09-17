b66c932edf45fd3b7c3e95dd62af874a1cffdd88
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-09-17 22:57:21 +0200

    feat(*): add linting to react app

---
+++ apps/react/eslint.config.mjs
@@ -0,0 +1,5 @@
+// @ts-check
+
+import { BASE_ESLINT_CONFIG, createEslintConfig } from "@wds/tools.eslint";
+
+export default createEslintConfig(BASE_ESLINT_CONFIG);
--- apps/react/src/index.tsx
+++ apps/react/src/index.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import { AppComponent } from "./app";
+
 import "./index.css";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("app_root")!).render(
   <StrictMode>
     <AppComponent />
-  </StrictMode>
+  </StrictMode>,
 );
