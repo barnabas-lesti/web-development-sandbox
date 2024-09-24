1ebf3e7c21412183c38019196f9b73002cc0c3d4
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-09-24 21:27:35 +0200

    feat(*): create input styles

--- packages/ui.stencil/src/components/input/input.component.tsx
+++ packages/ui.stencil/src/components/input/input.component.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from "@stencil/core";
+import { Component, h, Listen, Prop, State } from "@stencil/core";
 
 @Component({
   tag: "wds-input",
@@ -8,13 +8,40 @@ import { Component, h, Prop } from "@stencil/core";
 })
 export class InputComponent {
   @Prop({ reflect: true, mutable: true }) value: string;
+  @Prop() label?: string;
+
+  @State() private isFocused = false;
+
+  @Listen("focus")
+  focusEventHandler() {
+    this.isFocused = true;
+  }
+
+  @Listen("blur")
+  blurEventHandler() {
+    this.isFocused = false;
+  }
 
   render() {
     return (
-      <input
-        value={this.value}
-        onInput={(event) => (this.value = (event.target as HTMLInputElement).value)}
-      />
+      <div
+        class={{
+          "container": true,
+          "focused": this.isFocused,
+          "has-value": !!this.value,
+        }}
+      >
+        {this.label && <label>{this.label}</label>}
+        <div class="input-wrapper">
+          <input
+            value={this.value}
+            onInput={(event) => (this.value = (event.target as HTMLInputElement).value)}
+          />
+          <fieldset>
+            <legend>{this.label && <span>{this.label}</span>}</legend>
+          </fieldset>
+        </div>
+      </div>
     );
   }
 }
---
+++ packages/ui.stencil/src/components/input/input.stories.ts
@@ -0,0 +1,32 @@
+import { withActions } from "@storybook/addon-actions/decorator";
+import { type Meta, type StoryObj } from "@storybook/html";
+
+import { type InputComponent } from "./input.component";
+
+export default {
+  title: "wds-input",
+  decorators: [withActions],
+  parameters: {
+    actions: {
+      handles: ["input"],
+    },
+  },
+  argTypes: {
+    label: {
+      type: "string",
+      control: { type: "text" },
+    },
+    value: {
+      type: "string",
+      control: { type: "text" },
+    },
+  },
+  render: (args) => Object.assign(document.createElement("wds-input"), args),
+} as Meta<InputComponent>;
+
+export const Example: StoryObj<InputComponent> = {
+  args: {
+    label: "Lorem ipsum",
+    value: "",
+  },
+};
