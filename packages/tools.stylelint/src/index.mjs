1e05aa6a10e8dbf006b38e8c67de5be2fb219f9c
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-09-25 21:19:24 +0200

    feat(*): update stylelint rules

--- packages/tools.stylelint/src/index.mjs
+++ packages/tools.stylelint/src/index.mjs
@@ -6,7 +6,7 @@
  * https://stylelint.io/user-guide/rules
  * @type {import('stylelint').Config} */
 export const BASE_WORKSPACE_STYLELINT_CONFIG = {
-  extends: ["stylelint-config-recommended" /* "stylelint-config-clean-order" */],
+  extends: ["stylelint-config-recommended", "stylelint-scss", "stylelint-config-clean-order/error"],
   rules: {
     "no-empty-source": null,
     "color-no-hex": true,
