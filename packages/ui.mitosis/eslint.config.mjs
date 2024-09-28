4a460682cd4d6e087004cc8f8c4f594f55024743
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-09-28 20:22:20 +0200

    feat(*): update linters

--- packages/ui.mitosis/eslint.config.mjs
+++ packages/ui.mitosis/eslint.config.mjs
@@ -1,3 +1,3 @@
 import { BASE_WORKSPACE_ESLINT_CONFIG } from "@wds/tools.eslint";
 
-export default [{ ignores: ["targets"] }, ...BASE_WORKSPACE_ESLINT_CONFIG];
+export default BASE_WORKSPACE_ESLINT_CONFIG;
