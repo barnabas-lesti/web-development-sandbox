88ac977362d40e4efe68c85a63653923405326b5
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-09-28 14:52:48 +0200

    feat(*): add angular mitosis builder

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
 
-import { WdsButton } from "@wds/ui.mitosis/angular";
+import { WdsExample } from "@wds/ui.angular";
 
 @Component({
   selector: "app-component",
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   standalone: true,
-  imports: [RouterOutlet, FormsModule, WdsButton],
+  imports: [RouterOutlet, FormsModule, WdsExample],
 })
 export class AppComponent {
   foo() {
-    void 0;
+    void 1;
   }
 }
