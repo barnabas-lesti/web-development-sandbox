db44d547ef01be8b6d380226c88acf7467c63303
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-10-06 19:22:49 +0200

    chore(vike): lint files

--- apps/vike/renderer/Layout.tsx
+++ apps/vike/renderer/Layout.tsx
@@ -1,12 +1,14 @@
-export { Layout }
+export { Layout };
 
-import React from 'react'
-import logoUrl from './logo.svg'
-import { PageContextProvider } from './usePageContext'
-import { Link } from './Link'
-import type { PageContext } from 'vike/types'
-import './css/index.css'
-import './Layout.css'
+import React from "react";
+import type { PageContext } from "vike/types";
+
+import { Link } from "./Link";
+import logoUrl from "./logo.svg";
+import { PageContextProvider } from "./usePageContext";
+
+import "./css/index.css";
+import "./Layout.css";
 
 function Layout({ children, pageContext }: { children: React.ReactNode; pageContext: PageContext }) {
   return (
@@ -23,21 +25,21 @@ function Layout({ children, pageContext }: { children: React.ReactNode; pageCont
         </Frame>
       </PageContextProvider>
     </React.StrictMode>
-  )
+  );
 }
 
 function Frame({ children }: { children: React.ReactNode }) {
   return (
     <div
       style={{
-        display: 'flex',
+        display: "flex",
         maxWidth: 900,
-        margin: 'auto'
+        margin: "auto",
       }}
     >
       {children}
     </div>
-  )
+  );
 }
 
 function Sidebar({ children }: { children: React.ReactNode }) {
@@ -47,15 +49,15 @@ function Sidebar({ children }: { children: React.ReactNode }) {
       style={{
         padding: 20,
         flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        lineHeight: '1.8em',
-        borderRight: '2px solid #eee'
+        display: "flex",
+        flexDirection: "column",
+        lineHeight: "1.8em",
+        borderRight: "2px solid #eee",
       }}
     >
       {children}
     </div>
-  )
+  );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
@@ -66,13 +68,13 @@ function Content({ children }: { children: React.ReactNode }) {
         style={{
           padding: 20,
           paddingBottom: 50,
-          minHeight: '100vh'
+          minHeight: "100vh",
         }}
       >
         {children}
       </div>
     </div>
-  )
+  );
 }
 
 function Logo() {
@@ -80,12 +82,17 @@ function Logo() {
     <div
       style={{
         marginTop: 20,
-        marginBottom: 10
+        marginBottom: 10,
       }}
     >
       <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
+        <img
+          src={logoUrl}
+          height={64}
+          width={64}
+          alt="logo"
+        />
       </a>
     </div>
-  )
+  );
 }
--- apps/vike/renderer/types.ts
+++ apps/vike/renderer/types.ts
@@ -1,25 +1,26 @@
 // https://vike.dev/pageContext#typescript
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Vike {
     interface PageContext {
-      Page: () => React.ReactElement
+      Page: () => React.ReactElement;
       data?: {
         /** Value for <title> defined dynamically by by /pages/some-page/+data.js */
-        title?: string
+        title?: string;
         /** Value for <meta name="description"> defined dynamically */
-        description?: string
-      }
+        description?: string;
+      };
       config: {
         /** Value for <title> defined statically by /pages/some-page/+title.js (or by `export default { title }` in /pages/some-page/+config.js) */
-        title?: string
+        title?: string;
         /** Value for <meta name="description"> defined statically */
-        description?: string
-      }
+        description?: string;
+      };
       /** https://vike.dev/render */
-      abortReason?: string
+      abortReason?: string;
     }
   }
 }
 
 // Tell TypeScript this file isn't an ambient module
-export {}
+export {};
