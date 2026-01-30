d6f3c8ea3bae62d908c33f6b73675787d0538622
Barnabas Lesti <barnabas.lesti@gmail.com>
2026-01-30 14:27:32 +0100

    [feature] add prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Options} */
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "auto",
+  singleAttributePerLine: true,
+};
